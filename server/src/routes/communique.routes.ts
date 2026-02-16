0422993623b7db43cb412c94acd73ea68fcb56ec
CODECZERO <as8857981@gmail.com>
2026-02-16 12:12:29 +0530

    feat(server): update communique.routes.ts

--- server/src/routes/communique.routes.ts
+++ server/src/routes/communique.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { createCommunique, getDivisionCommuniques } from '../controler/communique.controler.js';
+import { verifyToken } from '../midelware/verify.midelware.js';
 
 const router = Router();
 
-router.post('/', createCommunique);
-router.get('/:ngoId', getDivisionCommuniques);
+router.post('/', verifyToken, createCommunique);
+router.get('/:ngoId', verifyToken, getDivisionCommuniques);
 
 export default router;
