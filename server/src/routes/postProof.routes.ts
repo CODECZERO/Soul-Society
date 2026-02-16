efd9d0001f8f66b62047d2e8d6994482a0454228
CODECZERO <as8857981@gmail.com>
2026-02-16 12:12:29 +0530

    feat(server): remove postProof.routes.ts

--- server/src/routes/postProof.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { submitProof, verifyProof } from '../controler/postProof.controler.js';
-
-const router = Router();
-
-router.post('/:id/submit-proof', submitProof);
-router.post('/:id/verify-proof', verifyProof);
-
-export default router;
