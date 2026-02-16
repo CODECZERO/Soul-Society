f2a0bc2e2290f685179d3c495982fb3c451be3cb
CODECZERO <as8857981@gmail.com>
2026-02-16 12:16:46 +0530

    feat(server): add jest.config.ts

---
+++ server/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+                tsconfig: 'tsconfig.json',
+            },
+        ],
+    },
+    testMatch: [
+        '<rootDir>/src/__tests__/**/*.test.ts',
+    ],
+    testTimeout: 30000,
+    verbose: true,
+    forceExit: true,
+    detectOpenHandles: true,
+};
+
+export default config;
