7cf3835d781aad3f27943318885b5eb7d008a287
CODECZERO <as8857981@gmail.com>
2026-02-18 10:18:01 +0530

    fix(frontend): chart-wrapper component

--- frontend/components/chart-wrapper.tsx
+++ frontend/components/chart-wrapper.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic"
 import { Skeleton } from "@/components/ui/skeleton"
 
 // Dynamically import the chart with no SSR
-const DynamicChart = dynamic(() => import("./ui/chart"), { 
+const DynamicChart = dynamic(() => import("./ui/chart").then((m) => m.ChartContainer as any), { 
   ssr: false,
   loading: () => <Skeleton className="h-[300px] w-full" />
 })
